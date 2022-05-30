88560cfba2a4def3ab35c312fe9e4ba0ef569694
Lajos Kovács <lajoskvcs@icloud.com>
2022-05-30 14:56:57 +0200

    feat: Refactored code to use mitt instead of TinyEmitter

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import * as path from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import * as path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-	build: {
+  build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/Modalis.ts'),
-      name: 'Modalis'
+      entry: path.resolve(__dirname, "src/Modalis.ts"),
+      name: "Modalis",
     },
     rollupOptions: {
-      external: ['vue', 'tiny-emitter'],
+      external: ["vue", "mitt"],
       output: {
         globals: {
-          vue: 'Vue',
-          'tiny-emitter': 'TinyEmitter'
-        }
-      }
-    }
-  }
-})
+          vue: "Vue",
+          mitt: "mitt",
+        },
+      },
+    },
+  },
+});
